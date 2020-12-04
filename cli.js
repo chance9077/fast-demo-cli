e8fc2f0da3a99e86bdd60006029be71cb99ca1eb
wangquanjiang <wangquanjiang@up366.com>
2020-12-04 17:33:08 +0800

    modify: add success tip

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk')
 const parseArgs = require('minimist')
 const { downloadTask } = require('./lib/task')
 
@@ -7,4 +8,8 @@ const params = parseArgs(process.argv.slice(2))
 const projectName = params._[0]
 
 downloadTask({ name: projectName })
+  .then(() => {
+    console.log(chalk.blue(`cd ${projectName}`))
+    console.log(chalk.blue(`npm or yarn run dev`))
+  })
 
